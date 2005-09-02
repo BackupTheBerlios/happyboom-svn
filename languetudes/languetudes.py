0bac707c4e6f11129ad6521d6ae3a2a63a4f1c2a
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-09-02 23:59:15 +0000

    Attrape les erreurs "module absent".

--- languetudes/languetudes.py
+++ languetudes/languetudes.py
@@ -18,7 +18,7 @@ ihm = ("console", "ncurse", "tkinter")
 def main():
     ihmModule = "tkinter"
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", "help, ihm=")
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "ihm="])
     except getopt.GetoptError:
         print "ERREUR: usage incorrect."
         print usage
@@ -34,9 +34,14 @@ def main():
                 print "ERREUR: ihm incorrect."
                 print usage
                 sys.exit(2)
-    src = __import__("src", globals(),  locals(), [ihmModule])
-    mod = getattr(src, ihmModule)
-    mod.Application()
+    try:
+        src = __import__("src", globals(),  locals(), [ihmModule])
+        mod = getattr(src, ihmModule)
+        mod.Application()
+    except ImportError, errmsg:
+        print "Erreur : un module Python est absent alors qu'il est nécessaire."
+        print errmsg
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
