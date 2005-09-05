7d9968ac11967732942764c31be17e3d93116cb1
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-09-05 00:01:49 +0000

    Support gettext

--- happyboom/trunk/happywarry/happywarry-server.py
+++ happyboom/trunk/happywarry/happywarry-server.py
@@ -2,27 +2,33 @@
 # -*- coding: UTF-8 -*-
 VERSION="0.0.0"
 PROGRAM="HappyWarry"
-
+import gettext
 
 def main():
     # Ajoute APIBoom au PYTHONPATH ("../" pour l'instant)
     import sys, os
-    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    apiboomdir = os.path.join(os.path.dirname(__file__), "..")
+    sys.path.append(apiboomdir)
 
-    # Cree le systeme de log
+    # Setup log
     from common.log import log
     log.setFilename("/home/haypo/.happywarry/server-log")
 
-    # Instancie une serveur
+    # Setup gettext
+    localedir = os.path.join(os.path.dirname(__file__), "./locale")
+    gettext.install('happywarry', localedir, unicode=1)
+
+    # Create the server 
     from server.hw_server import HappyWarryServer
     server = HappyWarryServer()    
-
+   
+    # Run the server
     try:
-        log.info("Server started.")
+        log.info(_("Server started."))
         server.run()
     except KeyboardInterrupt:
-        log.info("Program interrupted (CTRL+C).")
+        log.info(_("Program interrupted (CTRL+C)."))
     server.stop()
-    log.info("Server quit.")
+    log.info(_("Server quit."))
 
 if __name__=="__main__": main()
