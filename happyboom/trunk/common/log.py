0c94935b48bdb347fbcfd47d384e20bb37684829
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-09-05 00:28:31 +0000

    Comment log class.

--- happyboom/trunk/common/log.py
+++ happyboom/trunk/common/log.py
@@ -13,8 +13,17 @@ class Log:
         self.__file = None
 
     def setFilename(self, filename):
+        """
+        Use a file to store all messages. The
+        UTF-8 encoding will be used. Write an informative
+        message if the file can't be created.
+
+        @param filename: C{L{string}}
+        """
+
         try:
-            self.__file = open(filename, "w")
+            import codecs
+            self.__file = codecs.open(filename, "w", "utf-8")
         except IOError, errno:
             if errno[0] == 2:
                 self.__file = None
@@ -22,33 +31,58 @@ class Log:
                 return
             raise
 
-    def dump(self):
-        print self.__buffer
-
     def getLevelPrefix(self, level):
+        """
+        String prefix which depends on message level.
+        Eg. information returns "[info]".
+        @return: C{str}
+        """
         if level==Log.LOG_WARN: return "[warn]"
         if level==Log.LOG_ERROR: return "[error]"
         return "[info]"
 
     def new_message(self, level, str):
+        """
+        Write a new message : append it in the buffer,
+        display it to the screen (if needed), and write
+        it in the log file (if needed).
+
+        @param level: Message level.
+        @type level: C{int}
+        @param str: Message content.
+        @type str: C{str}
+        """
+        
         if not self.__buffer.has_key(level):
             self.__buffer[level] = [str]
         else:
             self.__buffer[level].append(str)
         print str
         if self.__file:
-            self.__file.write("%s - %s %s\n" \
+            self.__file.write(u"%s - %s %s\n" \
                 % (time.strftime("%Y-%M-%d %H:%M:%S"),
                    self.getLevelPrefix(level),
                    str))
 
     def info(self, str):
+        """
+        New informative message.
+        @type str: C{str}
+        """
         self.new_message(Log.LOG_INFO, str)
 
     def warn(self, str):
+        """
+        New warning message.
+        @type str: C{str}
+        """
         self.new_message(Log.LOG_WARN, str)
 
     def error(self, str):
+        """
+        New error message.
+        @type str: C{str}
+        """
         self.new_message(Log.LOG_WARN, str)
 
 log = Log()        
