70531d55a063edda3d267265a85ca32eab9e72bb
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-11-21 23:15:18 +0000

    Improve import.

--- haypo/mediawiki/import_pages.py
+++ haypo/mediawiki/import_pages.py
@@ -4,7 +4,6 @@
 import sys, os, re, types
 #from elementtree.ElementTree import ElementTree
 from xml.dom.minidom import parseString
-from xml.dom.minidom import parseString
 from xml.parsers.expat import ParserCreate
 from xml import sax
 from subprocess import Popen, PIPE
@@ -18,109 +17,159 @@ def usage():
     print "    => will use http://www.haypocalc.com/wiki/Special:Allpages"
     sys.exit(1)
 
-def escapeshellarg(arg):
-    # TODO (use os.<the right function>)
-    arg = arg.replace("\\", "\\\\").replace("'", "\\'")
-    return "'"+arg+"'"
-
 def error(msg):
     print msg
 
 def warning(msg):
     print msg
 
-def download(url, params={}):
-    params = urllib.urlencode(params)
-    f = urllib.urlopen(url, params)
-    return f.read()
+class Importer:
+    def __init__(self, base_url):
+        # Add "/" prefix to url if needed
+        if base_url[-1] != "/":
+            self.base_url = base_url+"/"
+        else:
+            self.base_url = base_url
 
-def getAllpages(url, namespace):
-    pages = download(url+"Special:Allpages", {"namespace": namespace})
-    if pages == None:
-        return None
+        self.only_last = False
+        self.dir = "pages"
+        self.use_gzip = True
+        self.max_per_file = 200 
+        
+    def download(self, page, params={}):
+        params = urllib.urlencode(params)
+        f = urllib.urlopen(self.base_url+page, params)
+        return f.read()
 
-    # Work around MediaWiki 1.4 bug (don't escape "&" chararacter)
-    pages = re.sub("&([^a-zA-Z])", r"&amp;\1", pages)
+    def getAllpages(self, namespace):
+        pages = self.download("Special:Allpages", {"namespace": namespace})
+        if pages == None:
+            return None
 
-    xml = parseString(pages)
-    root = xml.documentElement
-    tables = root.getElementsByTagName('table')
-    assert len(tables) == 2
-    table = tables[1]
-    links = table.getElementsByTagName('a')
-    pages = []
-    for link in links:
-        page = link.getAttribute('href').encode("ASCII")
-        page = page.split("/")
-        page = urllib.unquote(page[-1])
-        if type(page) == types.UnicodeType:
-            page = page.encode("UTF-8")            
-        pages.append( page )
-    return pages
+        # Work around MediaWiki 1.4 bug (don't escape "&" chararacter)
+        pages = re.sub("&([^a-zA-Z])", r"&amp;\1", pages)
 
-def importNamespace(url, dir, namespace, only_last):
-    print "Download document list."
-    pages = getAllpages(url, namespace)
-    if pages == None:
-        print "Error: Can't get document list."
-        sys.exit(1)
-    if len(pages) == 0:
-        print "(empty)"
-        return
-    print "Download XML data (%u documents) ..." % len(pages)
-    args = { \
-        "action": "submit",
-        "pages": "\n".join(pages)
-    }
-    if only_last:
-        args["curonly"] = "on"
-    data = download(url+"Special:Export", args)
-    if data == None:
-        error("Fail to download namespace %s." % namespace)
-        sys.exit(1)
-    filename = os.path.join(dir, "namespace_%s.xml" % namespace)
-    f = open(filename, 'w')
-    f.write(data)
-    f.close()
+        xml = parseString(pages)
+        root = xml.documentElement
+        tables = root.getElementsByTagName('table')
+        assert len(tables) == 2
+        table = tables[1]
+        links = table.getElementsByTagName('a')
+        pages = []
+        for link in links:
+            page = link.getAttribute('href').encode("ASCII")
+            page = page.split("/")
+            page = urllib.unquote(page[-1])
+            if type(page) == types.UnicodeType:
+                page = page.encode("UTF-8")            
+            pages.append( page )
+        return pages
 
-def importPages(url, only_last=False):
-    # Add "/" prefix to url if needed
-    if url[-1] != "/":
-        url = url + "/"
+    def savePages(self, namespace, part, content):
+        if part != None:
+            filename = "namespace_%s_part%u.xml" % (namespace, part)
+        else:
+            filename = "namespace_%s.xml" % namespace
+        filename = os.path.join(self.dir, filename)
+        if self.use_gzip:
+            filename = filename + ".gz"
+            f = GzipFile(filename, 'w')
+            f.write(content)
+            f.close()
+        else:
+            f = open(filename, 'w')
+            f.write(content)
+            f.close()
+        print "saved to %s" % (filename)
 
-    # Create pages/ if needed
-    dir = "pages"
-    try:
-      os.mkdir(dir)
-    except OSError, err:
-        if err[0] != 17:
-            raise
+    def importPages(self, namespace, pages, part):
+        args = { \
+            "action": "submit",
+            "pages": "\n".join(pages)
+        }
+        if self.only_last:
+            args["curonly"] = "on"
+        data = self.download("Special:Export", args)
+        if data == None:
+            error("Fail to download namespace %s." % namespace)
+            sys.exit(1)
+        self.savePages(namespace, part, data)
 
-    # Which namespaces have to be downloaded?
-    namespaces = range(0,16)
-    namespaces.remove(8) # MediaWiki
-    namespaces.remove(9) # Talk:MediaWiki
-    
-    # Download each namespace
-    i = 1
-    for namespace in namespaces: 
-        print "================== Import namespace %u (%u/%u) ============== " \
-                % (namespace, i, len(namespaces))
-        importNamespace(url, dir, namespace, only_last)
-        i = i + 1
+    def importNamespace(self,  namespace):
+        print "Download document list."
+        pages = self.getAllpages(namespace)
+        if pages == None:
+            print "Error: Can't get document list."
+            sys.exit(1)
+        if len(pages) == 0:
+            print "(empty)"
+            return
+        part = 1
+        position = 1
+        total = len(pages)
+        while len(pages) != 0:
+            if self.max_per_file != 0:
+                sub = pages[:self.max_per_file]
+                pages = pages[self.max_per_file:]
+            else:
+                sub = pages
+                pages = []
+            sys.stdout.write("Download document(s) %u..%u / %u ... " \
+                 % (position, position+len(sub)-1, total))
+            sys.stdout.flush()
+            try:
+                if total != len(sub):
+                    self.importPages(namespace, sub, part)
+                    part = part + 1
+                    position = position + len(sub)
+                else:
+                    part = None
+                    self.importPages(namespace, sub, part)
+            except:
+                sys.stdout.write("ERROR!\n\n")
+                raise
 
-def exportPages(url):
-    print "TODO"
+    def doImport(self):
+        # Which namespaces have to be downloaded?
+        namespaces = range(0,16)
+        namespaces.remove(8) # MediaWiki
+        namespaces.remove(9) # Talk:MediaWiki
 
-def main():
-    if len(sys.argv) != 2:
-        usage()
-    url = sys.argv[1]
-    try:
-        importPages(url)
-    except KeyboardInterrupt:
-        print "Interrupted (CTRL+C): be carefull, download incomplete!"
-        sys.exit(1)
+        # Sum up before starting
+        print "Base url: %s" % (self.base_url)
+        print "Store into gzip: %s" % (self.use_gzip)
+        print "Max documents/file: %s" % (self.max_per_file)
 
-if __name__=="__main__":
-    main()        
+        # Create pages/ if needed
+        if not os.access(self.dir, os.F_OK):
+            print "Create subdirectory %s" % (self.dir)
+            os.mkdir(self.dir)
+        print ""
+        
+        # Download each namespace
+        i = 1
+        for namespace in namespaces: 
+            namespace = 6
+            print "================== Import namespace %u (%u/%u) ============== " \
+                    % (namespace, i, len(namespaces))
+            self.importNamespace(namespace)
+            return
+            i = i + 1
+
+if len(sys.argv) != 2:
+    usage()
+base_url = sys.argv[1]
+try:
+    from gzip import GzipFile
+    use_gzip = True
+except ImportError:
+    warning("Can't load gzip module.")
+    use_gzip = False
+try:
+    my = Importer(base_url)
+    my.use_gzip = use_gzip
+    my.doImport()
+except KeyboardInterrupt:
+    print "Interrupted (CTRL+C): be carefull, download incomplete!"
+    sys.exit(1)
+print ""    
