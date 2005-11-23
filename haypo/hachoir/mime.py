3a3017384bfc05892e460f6466b5ac2c4f3341cb
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-11-23 01:51:32 +0000

    Add mime.py (may be used later)

---
+++ haypo/hachoir/mime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os, stat
+
+instance = None
+
+def getInstance():
+    global instance
+    if instance == None:
+        import magic
+        instance = magic.open(magic.MAGIC_MIME)
+        instance.load()
+    return instance
+
+def getFileMime(filename):
+    mode = os.stat(filename)[stat.ST_MODE]
+    if stat.S_ISDIR(mode):
+        return "Directory"
+    if stat.S_ISLNK(mode):
+        return "Link"
+    
+    f = file(filename, "rb")
+    buffer = f.read(4096)
+    f.close()
+    return getBufferMime(buffer)
+
+def getBufferMime(buffer):
+    magic = getInstance()
+    type = magic.buffer(buffer)
+    return type
