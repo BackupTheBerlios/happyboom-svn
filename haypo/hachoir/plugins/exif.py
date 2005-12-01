ba97ae1ba1b621ddf09d6cbdd4c701eb99daf50a
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-12-01 05:34:01 +0000

    Generalize IDF in Exif filter.

--- haypo/hachoir/plugins/exif.py
+++ haypo/hachoir/plugins/exif.py
@@ -18,9 +18,11 @@ class ExifEntry(Filter):
         3: (1, "H"),
         4: (1, "L"),
         5: (2, "L"),
-        7: (1, "B"),
-        9: (1, "l"),
-        10: (2, "l")
+        7: (1, "s"),
+#        9: (1, "l"),
+#        10: (2, "l")
+        9: (1, "L"),
+        10: (2, "L")
     }
 
     type_name = {    
@@ -67,8 +69,63 @@ class ExifEntry(Filter):
         0x0110: "Camera (Input input equipment) model",
         0x0131: "Software",
         0x013B: "File change date and time",
-        0x8298: "Copyright holder"
+        0x8298: "Copyright holder",
+        0x8769: "Exif IFD Pointer",
 
+        0x829A: "Exposure time",
+        0x829D: "F number",
+        0x8822: "Exposure program",
+        0x8824: "Spectral sensitivity",
+        0x8827: "ISO speed rating",
+        0x8828: "Optoelectric conversion factor OECF",
+        0x9201: "Shutter speed",
+        0x9202: "Aperture",
+        0x9203: "Brightness",
+        0x9204: "Exposure bias",
+        0x9205: "Maximum lens aperture",
+        0x9206: "Subject distance",
+        0x9207: "Metering mode",
+        0x9208: "Light source",
+        0x9209: "Flash",
+        0x920A: "Lens focal length",
+        0x9214: "Subject area",
+        0xA20B: "Flash energy",
+        0xA20C: "Spatial frequency response",
+        0xA20E: "Focal plane X resolution",
+        0xA20F: "Focal plane Y resolution",
+        0xA210: "Focal plane resolution unit",
+        0xA214: "Subject location",
+        0xA215: "Exposure index",
+        0xA217: "Sensing method",
+        0xA300: "File source",
+        0xA301: "Scene type",
+        0xA302: "CFA pattern",
+        0xA401: "Custom image processing",
+        0xA402: "Exposure mode",
+        0xA403: "White balance",
+        0xA404: "Digital zoom ratio",
+        0xA405: "Focal length in 35 mm film",
+        0xA406: "Scene capture type",
+        0xA407: "Gain control",
+        0xA408: "Contrast",
+
+        0x9000: "Exif version",
+        0xA000: "Supported Flashpix version",
+        0xA001: "Color space information",
+        0x9101: "Meaning of each component",
+        0x9102: "Image compression mode",
+        0xA002: "Valid image width",
+        0xA003: "Valid image height",
+        0x927C: "Manufacturer notes",
+        0x9286: "User comments",
+        0xA004: "Related audio file",
+        0x9003: "Date and time of original data generation",
+        0x9004: "Date and time of digital data generation",
+        0x9290: "DateTime subseconds",
+        0x9291: "DateTimeOriginal subseconds",
+        0x9292: "DateTimeDigitized subseconds",
+        0xA420: "Unique image ID",
+        0xA005: "Interoperability IFD Pointer"
     }
 
     def __init__(self, stream, parent, endian):
@@ -79,8 +136,7 @@ class ExifEntry(Filter):
         self.read("count", endian+"L", "Count")
 
         # Create format
-        assert self["type"] in ExifEntry.format
-        format = ExifEntry.format[ self["type"] ]
+        format = ExifEntry.format.get(self["type"], (1, "B"))
         self.format = "%s%u%s" % (self.endian, format[0]*self["count"], format[1])
 
         # Get size
@@ -107,6 +163,36 @@ class ExifEntry(Filter):
         chunk.description = self.getTag()
         return "0x%04X" % chunk.value 
 
+def sortExifEntry(a,b):
+    return int( a["offset"] - b["offset"] )
+class ExifIFD(Filter):
+    def __init__(self, stream, parent, endian, offset_diff):
+        Filter.__init__(self, "exif", "Exif IFD", stream, parent)
+        self.endian = endian
+        self.read("id", endian+"H", "IFD identifier")
+        entries = []
+        while True:
+            next = stream.getFormat("!L", False)[0]
+            if next == 0:
+                break
+            chunk = self.readChild("entry[]", ExifEntry, endian)
+            entry = chunk.getFilter()
+            if entry["tag"] == 0x8769:
+                break
+            if 4 < entry.size:
+                entries.append(entry)
+        self.read("next", endian+"L", "Next IFD offset")
+        entries.sort( sortExifEntry )                
+        for entry in entries:
+            offset = entry["offset"]+offset_diff
+            assert stream.tell() == offset
+            self.read("entry_value[]", entry.format, "Value of %s" % entry.getId())
+
+    def updateParent(self, chunk):
+        desc = "Exif IFD (id %s)" % self["id"]
+        chunk.description = desc
+        self.setDescription(desc)
+
 class ExifFilter(Filter):
     def __init__(self, stream, parent):
         Filter.__init__(self, "exif", "Exif", stream, parent)
@@ -122,22 +208,35 @@ class ExifFilter(Filter):
            endian = ">"
         self.read("header2", endian+"H", "Header2 (42)")
 
-        # Part #0
-        self.read("nb_entry", endian+"H", "Number of entries")
-        self.read("offset", endian+"L", "Reserved")
-        nb_entry = self["nb_entry"]
-        entries = []
-        for i in range(0,nb_entry):
-            chunk = self.readChild("entry[]", ExifEntry, endian)
-            entries.append(chunk.getFilter())
+        if False:
+            # Part #0
+            self.read("nb_entry", endian+"H", "Number of entries")
+            self.read("offset", endian+"L", "Offset")
+            nb_entry = self["nb_entry"]+1
+            entries = []
+            for i in range(0,nb_entry):
+                chunk = self.readChild("entry[]", ExifEntry, endian)
+                entry = chunk.getFilter()
+                if entry["tag"] != 0x8769:
+                    entries.append(entry)
 
-        # TODO: What's this?
-        self.read("whatisthat", "16s", "What's this?")
+            # TODO: What's this?
+            self.read("next", endian+"L", "Next IFD offset")
 
-        # Read data of part #0
-        for entry in entries:
-            if 4 < entry.size:
-                self.read("entry_value[]", entry.format, "Value of %s" % entry.getId())
+            # Read data of part #0
+            for entry in entries:
+                if 4 < entry.size:
+                    self.read("entry_value[]", entry.format, "Value of %s" % entry.getId())
 
-        # TODO: To be continued...
-        self.read("end", "{@end@}s", "End")
+            # Read IFD
+            self.readChild("ifd", ExifIFD, endian)
+        else:
+            self.read("nb_entry", endian+"H", "Number of entries")
+            self.read("whatsthis?", endian+"H", "What's this ??")
+            while True:
+                tag = stream.getN(2, False)
+                if tag == "\xFF\xFF":
+                    break
+                self.readChild("ifd[]", ExifIFD, endian, 6)
+        size = stream.getSize() - stream.tell()
+        self.read("end", "%us" % size, "End")
