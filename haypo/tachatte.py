d1715aa9f4b2da26fadd68677cc3dc8aa63393c8
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-12-06 01:09:22 +0000

    Add reversible mode to tachatte.

--- haypo/tachatte.py
+++ haypo/tachatte.py
@@ -6,6 +6,8 @@
 # Author: Victor Stinner
 
 import sys, re, random, string
+PROGRAM="Tachatte"
+VERSION="2005-12-06"
 
 class Word:
     def __init__(self, old, new):
@@ -13,27 +15,28 @@ class Word:
         self.new = new
 
 class Tachatter:
-    def __init__(self, file):
+    def __init__(self):
         # Options
         self.encode_number = True
         self.encode_string = True
+        self.reversible = False
         self.obscure = \
             ["tachatte", "zob", "couille", "merde",
              "poil", "grossepute", "putain", "encule",
              "batard", "tarace", "chameau"]
         
         # Attributes
-        self.word=None
+        self.uniq = 1
+        self.word = None
         self.thesaurus={}
         self.exclude = ["if", "else", "return", "for", "while", "do"]
-        self.file = file
+        self.file = None
         self.regex_c_include = re.compile("^include")
         self.c_include = ""
         self.start_word = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         if self.encode_number:
             self.start_word = self.start_word + "0123456789"
         self.content = []
-        self.process()
 
     def getContent(self):
         text = ""
@@ -46,9 +49,10 @@ class Tachatter:
 
     def getHeaders(self):
         content = self.c_include
-        for word in self.thesaurus:
-            item = self.thesaurus[word]
-            content = content + "#define %s %s\n" % (item.new, item.old)
+        if not self.reversible:
+            for word in self.thesaurus:
+                item = self.thesaurus[word]
+                content = content + "#define %s %s\n" % (item.new, item.old)
         return content
 
     def readComment(self):
@@ -73,13 +77,23 @@ class Tachatter:
         else:
             self.unput(str)
 
-    def process(self, c):
-        if self.word != None:
-            self.word = self.word + c
-        else:
-            self.word = c
+    def generateWordReversible(self, new_thesaurus):
+        self.uniq = self.uniq + 1
+        if 256 <= self.uniq:
+            raise Exception("No more shit!")
+        up="TACHATTE"
+        down="tachatte"
+        word = ""
+        index = self.uniq
+        for i in range(0,8):
+            if (index & 1 == 1):
+                word = word + up[i]
+            else:
+                word = word + down[i]
+            index = index/2
+        return word     
 
-    def generateWord(self, new_thesaurus):
+    def generateWordRandom(self, new_thesaurus):
         tries = 0
         while tries < 100:
             tries = tries + 1
@@ -116,7 +130,8 @@ class Tachatter:
         if c != None:
             self.unput(c)
 
-    def process(self):       
+    def process(self, file):
+        self.file = file
         while True:
             c = self.file.read(1)
             if c == '':
@@ -152,19 +167,61 @@ class Tachatter:
         new_words = {}
         for word in self.thesaurus:
             item = self.thesaurus[word]
-            item.new = self.generateWord(new_words)
+            if self.reversible:
+                item.new = self.generateWordReversible(new_words)
+            else:
+                item.new = self.generateWordRandom(new_words)
             new_words[item.new] = item
 
 def usage():
-    print "Usage: %s file.c" % sys.argv[0]
-    sys.exit(1)
+    print "%s version %s" % (PROGRAM, VERSION)
+    print ""
+    print "Usage: %s [options] file.c" % (sys.argv[0])
+    print
+    print "Options :"
+    print "\t--help            : Print this help"
+    print "\t--version         : Print the software version"
+    print "\t--reversible      : Use reversible mode"
+    sys.exit(2)
+
+def parseArgs(tachatte):
+    import getopt
+
+    try:
+        short = ""
+        long = ["help", "version", \
+            "reversible"]
+        opts, args = getopt.getopt(sys.argv[1:], short, long)
+    except getopt.GetoptError:
+        usage()
+        
+    for o, a in opts:
+        if o == "--help":
+            usage()
+        if o == "--version":
+            print "%s version %s" % (PROGRAM, VERSION)
+            sys.exit(0)
+        if o == "--reversible":
+            tachatte.reversible = True
+
+    if len(args) != 1:
+        usage()
+
+    return args[0]
 
 if __name__=="__main__":
     if len(sys.argv)<2:
         usage()
-    f=open(sys.argv[1])
+
+    # Prepare
+    t=Tachatter()
+    filename = parseArgs(t)
     random.seed()
-    t=Tachatter(f)
+
+    # Process file
+    file = open(filename)
+    t.process(file)
+
+    # Display result
     sys.stdout.write(t.getHeaders())
     sys.stdout.write(t.getContent())
-    f.close()
