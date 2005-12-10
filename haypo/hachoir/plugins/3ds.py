ff1ce8ca6cf1acc6ed19fb0e150a489c77df9bcf
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-12-10 15:26:24 +0000

    Fix 3DS plugin

--- haypo/hachoir/plugins/3ds.py
+++ haypo/hachoir/plugins/3ds.py
@@ -9,17 +9,17 @@ from plugin import registerPlugin
 def readTextureFilename(filter, stream):
     filter.readString("filename", "C", "Texture filename")
 
-def readVersion(filter, stream):
+def readVersion(filter, stream, last_pos):
     filter.read("version", "<L", "Version")
 
-def readMaterial(filter, stream):
+def readMaterialName(filter, stream, last_pos):
     filter.readString("name", "C", "Material name")
-    while not stream.eof():
-        filter.readChild("chunk[]", Filter_3DS_Chunk)
+#    while not stream.eof():
+#        filter.readChild("chunk[]", Filter_3DS_Chunk)
 
-def readObject(filter, stream):
-    filter.readString("name", "C", "Object name")
-    while not stream.eof():
+def readObject(filter, stream, last_pos):
+    chunk = filter.readString("name", "C", "Object name")
+    while stream.tell() < last_pos:
         filter.readChild("chunk[]", Filter_3DS_Chunk)
 
 class Filter_3DS_MapUV(Filter):
@@ -43,49 +43,92 @@ class Filter_3DS_Polygon(Filter):
         self.read("c", "<H", "Vertex C")
         self.read("flags", "<H", "Flags")
     
-def readMapList(filter, stream):
+def readMapList(filter, stream, last_pos):
     filter.read("count", "<H", "Map count")
-    while not stream.eof():
+    for i in range(0, filter["count"]):
         filter.readChild("map[]", Filter_3DS_MapUV)
 
-def readColor(filter, stream):
+def readColor(filter, stream, last_pos):
     filter.read("red", "B", "Red componant")
     filter.read("green", "B", "Green componant")
     filter.read("blue", "B", "Blue componant")
 
-def readVertexList(filter, stream):
+def readVertexList(filter, stream, last_pos):
     filter.read("count", "<H", "Vertex count")
-    while not stream.eof():
+    for i in range(0, filter["count"]):
         filter.readChild("vertex[]", Filter_3DS_Vertex)
     
-def readPolygonList(filter, stream):
+def readPolygonList(filter, stream, last_pos):
     filter.read("count", "<H", "Vertex count")
-    while not stream.eof():
+    for i in range(0, filter["count"]):
         filter.readChild("polygon[]", Filter_3DS_Polygon)
 
 class Filter_3DS_Chunk(Filter):
+    # List of chunk type name
+    type_name = {
+        0x0011: "Color",
+        0x4D4D: "Main chunk",
+        0x0002: "File version",
+        0x3D3D: "Materials and objects",
+        0x4000: "Object",
+        0x4100: "Mesh (triangular)",
+        0x4110: "Vertices list",
+        0x4120: "Polygon (faces) list",
+        0x4140: "Map UV list",
+        0x4130: "Object material",
+        0xAFFF: "New material",
+        0xA000: "Material name",
+        0xA010: "Material ambiant",
+        0xA020: "Material diffuse",
+        0xA030: "Texture specular",
+        0xA200: "Texture",
+        0xA300: "Texture filename",
+
+        # Key frames
+        0xB000: "Keyframes",
+        0xB002: "Object node tag",
+        0xB006: "Light target node tag",
+        0xB007: "Spot light node tag",
+        0xB00A: "Keyframes header",
+        0xB009: "Keyframe current time",
+        0xB030: "Node identifier",
+        0xB010: "Node header",
+        0x7001: "Viewport layout"
+    }
+
+    chunk_id_by_type = {
+        0x4d4d: "main",
+        0x0002: "version",
+        0x3d3d: "obj_mat",
+        0xb000: "keyframes",
+        0xafff: "material",
+        0x4000: "object"
+    }
+
     # List of chunks which contains other chunks
     sub_chunks = \
         (0x4D4D, 0x4100, 0x3D3D, 0xAFFF, 0xA200,
          0xB002, 0xB006, 0xB007,
          0xA010, 0xA030, 0xA020, 0xB000)
+
+    # List of chunk type handlers
+    handlers = {
+        0xA000: readMaterialName,
+        0x4000: readObject,
+        0xA300: readTextureFilename,
+        0x0011: readColor,
+        0x0002: readVersion,
+# TODO: Uncomment these functions, it's too slow yet            
+#         0x4110: readVertexList,
+#         0x4120: readPolygonList,
+#         0x4140: readMapList
+    }
     
     def __init__(self, stream, parent):
         Filter.__init__(self, "3ds_chunk", "3DS chunk", stream, parent)
         chunk = self.read("type", "<H", "Chunk type", post=self.toHex)
         chunk.description = "Chunk type (%s)" % self.getType()
         self.read("size", "<L", "Chunk size")
-        types = {
-            0xA000: readMaterial,
-#            0x4000: readObject,
-            0xA300: readTextureFilename,
-            0x0011: readColor,
-            0x0002: readVersion,
-# TODO: Uncomment these functions, it's too slow yet            
-#            0x4110: readVertexList,
-#            0x4120: readPolygonList,
-#            0x4140: readMapList
-        }
         size = self["size"] - 6
         type = self["type"] 
         end = stream.tell() + size
@@ -94,9 +137,10 @@ class Filter_3DS_Chunk(Filter):
                 self.readChild("chunk[]", Filter_3DS_Chunk)
             assert stream.tell() == end 
         else:
-            if type in types: 
-                substream = stream.createSub(size=size)
-                types[type] (self, substream)
+            if type in Filter_3DS_Chunk.handlers: 
+                end = stream.tell() + size
+                Filter_3DS_Chunk.handlers[type] (self, stream, end)
+                assert stream.tell() == end
             else:
                 self.read("data", "%us" % size, "Raw data")
 
@@ -107,47 +151,20 @@ class Filter_3DS_Chunk(Filter):
         type = self.getType()
         chunk.description = "Chunk of type \"%s\"" % type
         self.setDescription("Chunk type (%s)" % type)
-        self.setId("chunk_%04x" % self["type"])
+        if self["type"] in Filter_3DS_Chunk.chunk_id_by_type:
+            id = Filter_3DS_Chunk.chunk_id_by_type[self["type"]]
+#            chunk.id = id
+            self.setId(id) 
+        else:
+            self.setId("chunk_%04x" % self["type"])
+
 
     def toHex(self, chunk):
         return "%04X" % chunk.value
         
     def getType(self):
         type = self["type"]
-        know = {
-            0x0011: "Color",
-            0x4D4D: "Main chunk",
-            0x0002: "File version",
-            0x3D3D: "Materials and objects",
-            0x4000: "Object",
-            0x4100: "Mesh (triangular)",
-            0x4110: "Vertices list",
-            0x4120: "Polygon (faces) list",
-            0x4140: "Map UV list",
-            0x4130: "Object material",
-            0xAFFF: "New material",
-            0xA000: "New material (name)",
-            0xA010: "Material ambiant",
-            0xA020: "Material diffuse",
-            0xA030: "Texture specular",
-            0xA200: "Texture",
-            0xA300: "Texture filename",
-
-            # Key frames
-            0xB000: "Keyframes",
-            0xB002: "Object node tag",
-            0xB006: "Light target node tag",
-            0xB007: "Spot light node tag",
-            0xB00A: "Keyframes header",
-            0xB009: "Keyframe current time",
-            0xB030: "Node identifier",
-            0xB010: "Node header",
-            0x7001: "Viewport layout"
-        }
-        if type in know:
-            return know[type]
-        else:
-            return "%04X" % type 
+        return Filter_3DS_Chunk.type_name.get(type, "%04X" % type)
 
 class Filter_3DS_File(Filter):
     def __init__(self, stream, parent):
