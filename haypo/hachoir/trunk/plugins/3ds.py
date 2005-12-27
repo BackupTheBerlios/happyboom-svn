8f75a4e0655ad4da92b72f04b220060ad91c4bf3
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2005-12-27 17:57:40 +0000

    Upgrade 3DS plugin to new syntax

--- haypo/hachoir/trunk/plugins/3ds.py
+++ haypo/hachoir/trunk/plugins/3ds.py
@@ -3,65 +3,78 @@
 Author: Victor Stinner
 """
 
-from filter import Filter, OnDemandFilter
+from filter import OnDemandFilter, OnDemandFilter
+from chunk import FormatChunk, StringChunk
 from plugin import registerPlugin
 
 def readTextureFilename(filter, stream, last_pos):
-    filter.readString("filename", "C", "Texture filename")
+    filter.read("filename", "Texture filename", (StringChunk, "C"))
 
 def readVersion(filter, stream, last_pos):
-    filter.read("version", "<L", "Version")
+    filter.read("version", "Version", (FormatChunk, "uint32"))
 
 def readMaterialName(filter, stream, last_pos):
-    filter.readString("name", "C", "Material name")
+    filter.read("name", "Material name", (StringChunk, "C"))
 
 def readObject(filter, stream, last_pos):
-    chunk = filter.readString("name", "C", "Object name")
+    chunk = filter.read("name", "Object name", (StringChunk, "C"))
     while stream.tell() < last_pos:
-        filter.readChild("chunk[]", "Chunk", Filter_3DS_Chunk)
+        filter.read("chunk[]", "Chunk", (Filter_3DS_Chunk,))
 
-class Filter_3DS_MapUV(Filter):
+class Filter_3DS_MapUV(OnDemandFilter):
     def __init__(self, stream, parent):
-        Filter.__init__(self, "3ds_map", "3DS UV map", stream, parent)
-        self.read("u", "f", "Map U")
-        self.read("v", "f", "Map V")
+        OnDemandFilter.__init__(self, "3ds_map", "3DS UV map", stream, parent, "<")
+        self.read("u", "Map U", (FormatChunk, "float"))
+        self.read("v", "Map V", (FormatChunk, "float"))
 
-class Filter_3DS_Vertex(Filter):
+    def getStaticSize(stream, args):
+        return 4*2
+    getStaticSize = staticmethod(getStaticSize)
+ 
+class Filter_3DS_Vertex(OnDemandFilter):
     def __init__(self, stream, parent):
-        Filter.__init__(self, "3ds_vertex", "3DS vertex", stream, parent)
-        self.read("x", "f", "X")
-        self.read("y", "f", "Y")
-        self.read("z", "f", "Z")
+        OnDemandFilter.__init__(self, "3ds_vertex", "3DS vertex", stream, parent, "<")
+        self.read("x", "X", (FormatChunk, "float"))
+        self.read("y", "Y", (FormatChunk, "float"))
+        self.read("z", "Z", (FormatChunk, "float"))
 
-class Filter_3DS_Polygon(Filter):
+    def getStaticSize(stream, args):
+        return 4*3
+    getStaticSize = staticmethod(getStaticSize)
+
+class Filter_3DS_Polygon(OnDemandFilter):
     def __init__(self, stream, parent):
-        Filter.__init__(self, "3ds_polygon", "3DS polygon", stream, parent)
-        self.read("a", "<H", "Vertex A")
-        self.read("b", "<H", "Vertex B")
-        self.read("c", "<H", "Vertex C")
-        self.read("flags", "<H", "Flags")
+        OnDemandFilter.__init__(self, "3ds_polygon", "3DS polygon", stream, parent, "<")
+        self.read("a", "Vertex A", (FormatChunk, "uint16"))
+        self.read("b", "Vertex B", (FormatChunk, "uint16"))
+        self.read("c", "Vertex C", (FormatChunk, "uint16"))
+        self.read("flags", "Flags", (FormatChunk, "uint16"))
+
+    def getStaticSize(stream, args):
+        return 4*2
+    getStaticSize = staticmethod(getStaticSize)
     
 def readMapList(filter, stream, last_pos):
-    filter.read("count", "<H", "Map count")
+    filter.read("count", "Map count", (FormatChunk, "uint16"))
     for i in range(0, filter["count"]):
-        filter.readSizedChild("map[]", "Map UV", 2*4, Filter_3DS_MapUV)
+        filter.read("map[]", "Map UV", (Filter_3DS_MapUV,))
 
 def readColor(filter, stream, last_pos):
-    filter.read("red", "B", "Red componant")
-    filter.read("green", "B", "Green componant")
-    filter.read("blue", "B", "Blue componant")
+    filter.read("red", "Red componant", (FormatChunk, "uint8"))
+    filter.read("green", "Green componant", (FormatChunk, "uint8"))
+    filter.read("blue", "Blue componant", (FormatChunk, "uint8"))
 
 def readVertexList(filter, stream, last_pos):
-    filter.read("count", "<H", "Vertex count")
+    filter.read("count", "Vertex count", (FormatChunk, "uint16"))
     for i in range(0, filter["count"]):
-        filter.readSizedChild("vertex[]", "Vertex", 3*4, Filter_3DS_Vertex)
+        filter.read("vertex[]", "Vertex", (Filter_3DS_Vertex,))
     
 def readPolygonList(filter, stream, last_pos):
-    filter.read("count", "<H", "Vertex count")
+    filter.read("count", "Vertex count", (FormatChunk, "uint16"))
     for i in range(0, filter["count"]):
-        filter.readSizedChild("polygon[]", "Polygon", 4*2, Filter_3DS_Polygon)
+        filter.read("polygon[]", "Polygon", (Filter_3DS_Polygon,))
     while stream.tell() < last_pos:
-        filter.readChild("chunk[]", "Chunk", Filter_3DS_Chunk)
+        filter.read("chunk[]", "Chunk", (Filter_3DS_Chunk,))
 
 class Filter_3DS_Chunk(OnDemandFilter):
     # List of chunk type name
@@ -102,7 +115,11 @@ class Filter_3DS_Chunk(OnDemandFilter):
         0x3d3d: "obj_mat",
         0xb000: "keyframes",
         0xafff: "material",
-        0x4000: "object"
+        0x4000: "object",
+        0x4110: "vertices_list",
+        0x4120: "polygon_list",
+        0x4140: "mapuv_list",
+        0x4100: "mesh"
     }
 
     # List of chunks which contains other chunks
@@ -118,23 +135,22 @@ class Filter_3DS_Chunk(OnDemandFilter):
         0xA300: readTextureFilename,
         0x0011: readColor,
         0x0002: readVersion,
-# TODO: Uncomment these functions, it's too slow yet            
-         0x4110: readVertexList,
-         0x4120: readPolygonList,
-         0x4140: readMapList
+        0x4110: readVertexList,
+        0x4120: readPolygonList,
+        0x4140: readMapList
     }
     
     def __init__(self, stream, parent):
-        OnDemandFilter.__init__(self, "3ds_chunk", "3DS chunk", stream, parent)
-        chunk = self.doRead("type", "<H", "Chunk type", post=self.toHex)
+        OnDemandFilter.__init__(self, "3ds_chunk", "3DS chunk", stream, parent, "<")
+        chunk = self.doRead("type", "Chunk type", (FormatChunk, "uint16"), {"post": self.toHex})
         chunk.description = "Chunk type (%s)" % self.getType()
-        self.read("size", "<L", "Chunk size")
+        self.read("size", "Chunk size", (FormatChunk, "uint32"))
         size = self["size"] - 6
         type = self["type"] 
         end = stream.tell() + size
         if type in Filter_3DS_Chunk.sub_chunks:
             while stream.tell() < end:
-                self.readChild("chunk[]", "Chunk", Filter_3DS_Chunk)
+                self.read("chunk[]", "Chunk", (Filter_3DS_Chunk,))
             assert stream.tell() == end 
         else:
             if type in Filter_3DS_Chunk.handlers: 
@@ -142,22 +158,19 @@ class Filter_3DS_Chunk(OnDemandFilter):
                 Filter_3DS_Chunk.handlers[type] (self, stream, end)
                 assert stream.tell() == end
             else:
-                self.read("data", "%us" % size, "Raw data")
+                self.read("data", "Raw data", (FormatChunk, "string[%u]" % size))
 
     def checkEnd(self, stream, array, last):
         return stream.eof()
 
     def updateParent(self, chunk):
         type = self.getType()
-        chunk.description = "Chunk of type \"%s\"" % type
-        self.setDescription("Chunk type (%s)" % type)
+        chunk.description = "Chunk: %s" % type
         if self["type"] in Filter_3DS_Chunk.chunk_id_by_type:
             id = Filter_3DS_Chunk.chunk_id_by_type[self["type"]]
-# TODO: Re-enable that            
-#            chunk.id = id
-            self.setId(id) 
+            chunk.id = id
         else:
-            self.setId("chunk_%04x" % self["type"])
+            chunk.id = "chunk_%04x" % self["type"]
 
     def toHex(self, chunk):
         return "%04X" % chunk.value
@@ -166,10 +179,10 @@ class Filter_3DS_Chunk(OnDemandFilter):
         type = self["type"]
         return Filter_3DS_Chunk.type_name.get(type, "%04X" % type)
 
-class Filter_3DS_File(Filter):
+class Filter_3DS_File(OnDemandFilter):
     def __init__(self, stream, parent):
-        Filter.__init__(self, "3ds_file", "3DS file", stream, parent)
+        OnDemandFilter.__init__(self, "3ds_file", "3DS file", stream, parent)
         while not stream.eof():
-            self.readChild("chunk[]", Filter_3DS_Chunk)
+            self.read("chunk[]", "Chunk", (Filter_3DS_Chunk,))
 
 registerPlugin(Filter_3DS_File, "image/x-3ds")
--- haypo/hachoir/trunk/text_handler.py
+++ haypo/hachoir/trunk/text_handler.py
@@ -1,21 +1,21 @@
+import datetime
+from tools import humanFilesize as doHumanFilesize
+
 def msdosDatetime(chunk):
-    import datetime
     assert chunk.size == 4
     val = chunk.value
-    sec = 2 * (val & 31)              # 5 bits: second
+    sec = 2 * (val & 31)              # 5 bits: second
     min = (val >> 5) & 63             # 6 bits: minute
-    hour = (val >> 11) & 31           # 5 bits: hour
+    hour = (val >> 11) & 31           # 5 bits: hour
     day = (val >> 16) & 31            # 5 bits: day
     month = (val >> 21) & 15          # 4 bits: month
     year = 1980 + ((val >> 25) & 127) # 7 bits: year
     return str(datetime.datetime(year, month, day, hour, min, sec))
     
 def humanFilesize(chunk):
-    from tools import humanFilesize as doHumanFilesize
     return doHumanFilesize(chunk.value)
 
 def unixTimestamp(chunk):
-    import datetime
     timestamp = datetime.datetime.fromtimestamp(chunk.value)
     return str(timestamp) 
 
