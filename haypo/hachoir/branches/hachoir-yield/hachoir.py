4483c19c5c75f1d44d3ea609a429254ebd64e4e0
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2006-02-28 11:56:53 +0000

    r5@haypopc:  haypo | 2006-02-28 12:53:54 +0100  Remove unused function (valueChanged)

--- haypo/hachoir/branches/hachoir-yield/hachoir.py
+++ haypo/hachoir/branches/hachoir-yield/hachoir.py
@@ -4,9 +4,6 @@ from file.image.png import PngFile
 from stream.file import FileStream
 from text_ui import displayFieldSet
 
-def valueChanged(field):
-    print "Value of %s changed to %s" % (field._name, field.value)
-
 def main():
     filename = "/home/haypo/exemple/png.png"
     stream = FileStream(open(filename, 'r'), filename)
