e78497a5fa27895ec2b3626c5203818c480bf477
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2006-03-04 00:29:54 +0000

    r29@haypopc:  haypo | 2006-03-03 20:38:33 +0100   * Split documentation in four files

--- haypo/hachoir/branches/hachoir-yield/test_doc.py
+++ haypo/hachoir/branches/hachoir-yield/test_doc.py
@@ -1,16 +1,23 @@
 import doctest, sys, os
+from stat import S_ISREG, ST_MODE
 
 def testDoc(filename, name=None):
-    print "Run test in %s" % filename
+    print "--- %s: Run tests" % filename
     doctest.testfile(filename, optionflags=doctest.ELLIPSIS, name=name)
-    print "End of all tests of %s" % filename
+    print "--- %s: End of tests" % filename
 
 def main():
     root_dir = os.path.dirname(__file__)
     hachoir_path = "libhachoir"
     sys.path.append(hachoir_path)
 
-    testDoc('doc/doc.txt', "Main document")
+    doc_dir = 'doc'
+    documents = os.listdir(doc_dir)
+    documents.sort()
+    for doc in documents:
+        filename = os.path.join(doc_dir, doc)
+        if S_ISREG(os.stat(filename)[ST_MODE]):
+            testDoc(filename)
 
 if __name__ == "__main__":
     main()
