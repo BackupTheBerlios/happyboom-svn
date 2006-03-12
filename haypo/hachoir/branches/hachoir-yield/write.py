19886f3fb714b71092333c0d6d169fd336a89d82
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2006-03-12 20:04:29 +0000

    r64@haypopc:  haypo | 2006-03-12 21:00:44 +0100   * Add dumb script to test write feature

---
+++ haypo/hachoir/branches/hachoir-yield/write.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys, os, getopt
+from text_ui import displayFieldSet
+
+def main():
+    current_dir = os.path.dirname(__file__)
+    libhachoir_path = os.path.join(current_dir, "libhachoir")
+    sys.path.append(libhachoir_path)
+
+    from libhachoir.parser.archive.gzip import GzipFile
+    from libhachoir.stream import FileInputStream, FileOutputStream
+
+    input = FileInputStream("/home/haypo/exemple/gz.gz")
+    gzip = GzipFile(None, "gzip", input)
+
+#    displayFieldSet(gzip)
+    
+    print "Set has_comment=True"
+    gzip["has_comment"].value = True 
+    print "Set has_comment=True: done"
+
+    output = FileOutputStream("/home/haypo/new.gz")
+    gzip.writeInto(output)
+#    displayFieldSet(gzip)
+    
+main()
