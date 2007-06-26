fa8c5978c93aed5c98cd5815e8fe413956e92ee9
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2007-06-26 23:03:42 +0000

    Smarter pypi output

--- haypo/lamer/mod_pypi.sh
+++ haypo/lamer/mod_pypi.sh
@@ -3,6 +3,4 @@ export MOD=pypi
 IN=~/.pypirc
 if [ ! -e $IN ]; then exit 0; fi
 tool/common.sh
-echo "http://cheeseshop.python.org/"
-echo $(grep username $IN)
-echo $(grep password $IN)
+echo "http://cheeseshop.python.org/ -- $(grep username $IN) -- $(grep password $IN)"
