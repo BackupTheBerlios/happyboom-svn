0a4d7eba4e8b9c1cc1b8e4723612a1ca08dbc84e
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2007-06-27 16:55:56 +0000

    Fix mod_svn: skip account without password

--- haypo/lamer/mod_svn.py
+++ haypo/lamer/mod_svn.py
@@ -7,11 +7,7 @@ class Subversion(Lamer):
         Lamer.__init__(self, "subversion", verbose)
 
     def parse(self, filename):
-        infos = {
-            'username': None,
-            'password': None,
-            'svn:realmstring': None,
-        }
+        infos = {}
         next = None
         skip = False
         for line in open(filename):
@@ -19,19 +15,22 @@ class Subversion(Lamer):
                 skip = False
                 continue
             line = line.strip()
-            if line in infos.keys():
+            if line in ('username', 'password', 'svn:realmstring'):
                 next = line
                 skip = True
             elif next:
                 infos[next] = line
                 next = None
-        server = infos["svn:realmstring"]
-        if not server:
-            return
-        server = re.sub("^<(.+)> .+$", r"\1", server)
+        try:
+            server = infos["svn:realmstring"]
+            if not server:
+                return
+            server = re.sub("^<(.+)> .+$", r"\1", server)
 
-        self.write("svn co %s --username=%s --password=%s" % (
-            server, infos["username"], infos["password"]))
+            self.write("svn co %s --username=%s --password=%s" % (
+                server, infos["username"], infos["password"]))
+        except KeyError:
+            return
 
     def _extract(self):
         for filename in self.glob("~/.subversion/auth/svn.simple/*"):
