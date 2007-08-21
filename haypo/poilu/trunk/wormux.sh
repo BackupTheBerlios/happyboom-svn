61fd8daa729e2c0f23fd4b4a9a2020c60fdd6136
haypo <haypo@decc8154-9ff4-0310-a513-87e9357441bb>
2007-08-21 20:58:53 +0000

    Remove old projects

--- haypo/poilu/trunk/wormux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -x
-python pypoil.py irc.eu.freenode.net:6667 '#poilu:utf8' geekouz 
--- haypo/turing/trunk/exception.py
+++
@@ -1,3 +0,0 @@
-class TuringException(Exception):
-	def __init__(self, msg):
-		Exception.__init__(self, msg)
